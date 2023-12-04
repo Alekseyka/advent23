a0597453945ffeae423e00a33eee4d81dad496b7
Serhii Balbieko <serhii@creditninja.com>
2023-12-04 01:52:12 -0500

    day4

--- m9/4/solution4-2.rb
+++ m9/4/solution4-2.rb
@@ -1,5 +1,13 @@
 #!/usr/bin/env ruby
 
+# Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+# Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+# Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+# Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+# Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+# Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+
+
 require 'pry'
 require 'awesome_print'
 require_relative '../lib/advent_of_code_client.rb'
@@ -8,12 +16,20 @@ if File.zero? 'input.txt'
   File.write 'input.txt', AdventOfCodeClient.new.get_task_input('4')
 end
 
-# input = File.read('input.txt')
-input = File.read('example.txt')
+input = File.read('input.txt')
+# input = File.read('example.txt')
 
-input
-  .each_line
-# .sum {_1.to_i }
-# .map(&:to_i)
-# .inject(:*)
-# .tap { ap _1  }
+
+input.each_line.with_object(Hash.new(0)) do |line, c|
+  gs, ls, rs = line.split(/[|:]/)
+
+  g = gs.scan(/\d+/).first.to_i
+  l = ls.scan(/\d+/)
+  r = rs.scan(/\d+/)
+
+  m = (l.intersection(r)).count
+
+  c[g] += 1
+
+  m.times { c[g + _1 + 1] += c[g] }
+end.values.sum.tap { puts _1 }
