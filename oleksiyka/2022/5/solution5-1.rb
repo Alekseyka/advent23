3873870ba6db8bce137e57e8bbd46c9cc0011785
Leks <leks@creditninja.com>
2023-12-03 23:01:32 -0500

    + 2022.5(1,2)

---
+++ oleksiyka/2022/5/solution5-1.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+require 'awesome_print'
+
+result = ''
+stacks = []
+stacks_number = 9
+
+File.foreach('input.txt') do |line|
+  # Fill up the stacks
+  if line.include?('[')
+    stacks_number.times do |stack_index|
+      stack_space = line[4 * stack_index, 3]
+      stacks[stack_index] = stacks[stack_index].to_a.unshift stack_space.to_s[1] unless stack_space.to_s.strip.empty?
+    end
+  end
+
+  if line.include?('move')
+    scan = line.scan(/move (\d+) from (\d+) to (\d+)/)
+    scan = scan[0].map(&:to_i)
+    scan[0].times do |_i|
+      from = scan[1] - 1
+      to = scan[2] - 1
+      item = stacks[from].pop
+      stacks[to].push(item)
+    end
+  end
+
+end
+
+puts "Result: '#{stacks.map { |s| s[-1] }.join('')}'"
