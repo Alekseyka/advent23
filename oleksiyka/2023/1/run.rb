8a3794d59d48221aa90c437bda90cd1bd4a526d4
Leks <leks@creditninja.com>
2023-12-06 00:34:55 -0500

    rba

--- oleksiyka/2023/1/run.rb
+++ oleksiyka/2023/1/run.rb
@@ -15,16 +15,17 @@ list = map.keys.map(&:to_s) + map.values.map(&:to_s)
 puts list
 rlist = list.map &:reverse
 
-File.foreach("in.txt") do |line|
+File.foreach('in.txt') do |line|
   first = line[Regexp.union(list)]
   first_n = first =~ /[0-9]/ ? first : map[first.to_sym]
-  first_n = first_n.to_i  
+  first_n = first_n.to_i
 
   last = line.reverse[Regexp.union(rlist)]
   last_n = last =~ /[0-9]/ ? last : map[last.reverse.to_sym]
   last_n = last_n.to_i
 
-  number = first_n*10+last_n
-  sum+=number
+  number = first_n * 10 + last_n
+  sum += number
 end
+
 puts sum
