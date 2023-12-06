ba71200762fb443fa0d0ae34838762499c3b2513
Leks <leks@creditninja.com>
2023-12-06 00:17:43 -0500

    + 2023.6(1,2)

---
+++ oleksiyka/2023/6/solution6-1.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+require 'awesome_print'
+require 'pry'
+
+the_arr = {
+  times: [],
+  distances: []
+}
+win_count = 1
+
+File.foreach('input.txt') do |line|
+  the_arr[:times] = line.scan(/\d+/).map(&:to_i) if line.include?('Time')
+  the_arr[:distances] = line.scan(/\d+/).map(&:to_i) if line.include?('Distance')
+end
+
+the_arr[:times].each.with_index do |time, index|
+  this_race_wins = 0
+  time.times do |seconds|
+    if (time - seconds) * seconds > the_arr[:distances][index]
+      this_race_wins += 1
+    end
+  end
+  win_count *= this_race_wins
+end
+
+ap the_arr
+ap win_count
+
