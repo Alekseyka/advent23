6a5471da5d695fa321cc2be1febf1eb01cf8c3ec
Leks <leks@creditninja.com>
2023-12-07 02:12:06 -0500

    + 2023.7(1,2??)

--- oleksiyka/2023/7/solution7-2.rb
+++ oleksiyka/2023/7/solution7-2.rb
@@ -4,25 +4,25 @@ require 'awesome_print'
 
 result = 0
 
-STRONGEST_COMBINATIONS_BY_ACTIVE_CARDS = {
+strongest_combinations_by_active_cards = {
   '5': :five_kind,
   '4': :four_kind,
   '3': :three_kind,
   '2': :one_pair,
   '1': :higher_card
 }.freeze
-ACTIVE_CARDS = {
+active_cards = {
   five_kind: 5, four_kind: 4, full_house: 5, three_kind: 3, two_pair: 4, one_pair: 2, higher_card: 1
 }.freeze
-WIN_CONDITIONS = {
+win_conditions = {
   five_kind: 700, four_kind: 600, full_house: 500, three_kind: 400, two_pair: 300, one_pair: 200, higher_card: 100
 }.freeze
-WIN_CARDS = {
-  'A': 14, 'K': 13, 'Q': 12, 'J': 1, 'T': 10, '9': 9, '8': 8, '7': 7, '6': 6, '5': 5, '4': 4, '3': 3, '2': 2
+win_cards = {
+  'a': 14, 'k': 13, 'q': 12, 'j': 1, 't': 10, '9': 9, '8': 8, '7': 7, '6': 6, '5': 5, '4': 4, '3': 3, '2': 2
 }.freeze
 
 def calculate_hand(combination, jokers, hand)
-  ap "Hand: #{hand}, jokers: #{jokers}, comb: #{combination}"
+  ap "hand: #{hand}, jokers: #{jokers}, comb: #{combination}"
   return combination if jokers == 0
   return :four_kind if combination == :two_pair && jokers == 2
   return :full_house if combination == :two_pair && jokers == 1
@@ -30,18 +30,18 @@ def calculate_hand(combination, jokers, hand)
   return :two_pair if combination == :one_pair && jokers == 1
   return :three_kind if combination == :one_pair && jokers == 2
 
-  ac = ACTIVE_CARDS[combination] + jokers
+  ac = active_cards[combination] + jokers
   ac = 5 if ac > 5
-  STRONGEST_COMBINATIONS_BY_ACTIVE_CARDS[ac.to_s.to_sym]
+  strongest_combinations_by_active_cards[ac.to_s.to_sym]
 end
 
 hands = []
 
-File.foreach('input.txt') do |line|
+file.foreach('input.txt') do |line|
   scan = line.scan(/(\w{5}) (\d+)/).map { |i| { hand: i[0], bid: i[1].to_i } }[0]
 
   sorted_hand = scan[:hand].chars.sort.join
-  joker_count = sorted_hand.gsub(/[^J]/, '').length
+  joker_count = sorted_hand.gsub(/[^j]/, '').length
 
   the_combination = case true
                     when !(sorted_hand =~ /(.)\1{4}/).nil?
@@ -60,14 +60,14 @@ File.foreach('input.txt') do |line|
                       :higher_card
                     end
   new_combination = calculate_hand(the_combination, joker_count, sorted_hand)
-  ap "Hand: #{sorted_hand}, jokers: #{joker_count}, comb: #{the_combination} --> #{new_combination}"
-  scan[:sort0] = WIN_CONDITIONS[new_combination]
+  ap "hand: #{sorted_hand}, jokers: #{joker_count}, comb: #{the_combination} --> #{new_combination}"
+  scan[:sort0] = win_conditions[new_combination]
 
-  scan[:sort1] = WIN_CARDS[scan[:hand][0].to_sym]
-  scan[:sort2] = WIN_CARDS[scan[:hand][1].to_sym]
-  scan[:sort3] = WIN_CARDS[scan[:hand][2].to_sym]
-  scan[:sort4] = WIN_CARDS[scan[:hand][3].to_sym]
-  scan[:sort5] = WIN_CARDS[scan[:hand][4].to_sym]
+  scan[:sort1] = win_cards[scan[:hand][0].to_sym]
+  scan[:sort2] = win_cards[scan[:hand][1].to_sym]
+  scan[:sort3] = win_cards[scan[:hand][2].to_sym]
+  scan[:sort4] = win_cards[scan[:hand][3].to_sym]
+  scan[:sort5] = win_cards[scan[:hand][4].to_sym]
   hands.push(scan)
 end
 
@@ -80,4 +80,4 @@ end
 # ap sorted_hands
 # ap hands
 # ap num_hands
-puts "Result: '#{result}'"
+puts "result: '#{result}'"
