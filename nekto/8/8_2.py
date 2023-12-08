dd884f6bddc2476e9e72475df08e8c2860912b54
Volodymyr K <vladimir.komlev@vidible.tv>
2023-12-08 17:22:44 -0500

    Update 8_2.py

--- nekto/8/8_2.py
+++ nekto/8/8_2.py
@@ -1,4 +1,5 @@
-
+from itertools import cycle
+from math import lcm
 
 def is_key_ends_with_A(pair):
     key, value = pair
@@ -7,31 +8,26 @@ def is_key_ends_with_A(pair):
     else:
         return False
    
-def check_all_positions(positions):
-    for position in positions:
-        if position[-1] != "Z":
-            return False
-    return True
-     
 file = open("8/input", "r")
-numer_of_steps = 0
+
 path = file.readline().rstrip()
 file.readline()
 path_points = {}
+loop_sizes = []
 
 for line in file:
     path_points[line[0:3]] = {"L": line[7:10], "R": line[12:15]}
 
 positions = dict(filter(is_key_ends_with_A, path_points.items()))
 print(positions)
-print(check_all_positions(positions.keys()))
-for step in cycle(path):
-    if check_all_positions(positions.keys()) == True:
-        break
-    new_positions = {}
-    for position in positions:
-        new_positions[positions.get(position).get(step)] = path_points.get(positions.get(position).get(step))
-    positions = new_positions
-    numer_of_steps +=1
+for position in positions:
+    numer_of_steps = 0
+    for step in cycle(path):
+        if position[2] == "Z":
+            loop_sizes.append(numer_of_steps)
+            break
+        position = path_points.get(position).get(step)
+        numer_of_steps +=1
 
-print(numer_of_steps)
+print(loop_sizes)
+print(lcm(*loop_sizes))
