d30bb2e70d34df0c8d219d3377880b4f4061d874
nekto <nekto@MacBook-Air.local>
2023-12-10 20:20:21 -0500

    dded nekto folder

--- nekto/1/1.py
+++ nekto/1/1.py
@@ -40,56 +40,38 @@ dicts_backward = {
     "5_full": set(["eerht", "neves", "thgie"]),
 }
 
-
-def find_first_digit_in_line(line: str):
+def find_digit_in_line(line: str, dict: dict, words_to_numbers: dict):
     word_length = 0
     word = ""
     for i in line:
         if i.isdigit():
             return int(i)
+
         word += i
         word_length += 1
-        if word in dicts_forward.get(str(word_length) + "_full", set()):
-            return words_to_numbers_forward[word]
-        if word in dicts_forward.get(word_length, set()):
+        if word in dict.get(str(word_length) + "_full", set()):
+            return words_to_numbers[word]
+        if word in dict.get(word_length, set()):
             continue
+
+        not_word = word
         word = i
-        word_length = 1
+        word_length = len(word)
+        for char in not_word[-2::-1]:
+            if char + word in dict.get(word_length + 1, set()):
+                word = char + word
+                word_length = len(word)
+                continue
+            else:
+                break
 
-
-def find_last_digit_in_line(line: str):
-    word_length = 0
-    word = ""
-    for i in line:
-        if i.isdigit():
-            return int(i)
-        word += i
-        word_length += 1
-        if word in dicts_backward.get(str(word_length) + "_full", set()):
-            return words_to_numbers_backward[word]
-        if word in dicts_backward.get(word_length, set()):
-            continue
-        word = i
-        word_length = 1
-
-
-out = open("1/output", "w")
-with open("1/input") as file:
+with open("nekto/1/input") as file:
     doc_checksum = 0
     for line in file:
         line = line.rstrip()
-        first_digit = find_first_digit_in_line(line)
+        first_digit = find_digit_in_line(line, dicts_forward, words_to_numbers_forward)
         doc_checksum += first_digit * 10
-        last_digit = find_last_digit_in_line(line[::-1])
+        last_digit = find_digit_in_line(line[::-1], dicts_backward, words_to_numbers_backward)
         doc_checksum += last_digit
-        out.write(
-            "{line}, {line_checksum}, {doc_checksum}\n".format(
-                line=line,
-                line_checksum=first_digit * 10 + last_digit,
-                doc_checksum=doc_checksum,
-            )
-        )
-out.close()
-
 
 print(doc_checksum)
