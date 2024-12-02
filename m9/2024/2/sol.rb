2d30ac5c4a6274025e86e014d017e1035d92f0d6
Serhii Balbieko <serhii@balbieko.com>
2024-12-02 01:11:43 -0500

    day2-sol1

---
+++ m9/2024/2/sol.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+
+require_relative '../common'
+
+$get.day(2)
+
+input     = File.read($o[:test] ? 'example.txt' : 'input.txt')
+input.each_line
+     .map {
+       _1.split.map(&:to_i)
+     }
+     .map {
+       _1 if ((_1 == _1.sort) || (_1 == _1.sort.reverse)) && (_1 == _1.uniq)
+     }
+     .compact
+     .map {
+       _1.each_cons(2).map { |a, b| (a - b).abs }.max
+     }
+     .map {_1 <= 3 || nil}
+     .compact.length
+     .tap {ap _1}
