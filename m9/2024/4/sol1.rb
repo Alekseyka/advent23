f9f9341131b661ebd61741a03dca812b7cf651df
Serhii Balbieko <serhii@balbieko.com>
2024-12-04 00:41:50 -0500

    cleanup

--- m9/2024/4/sol1.rb
+++ m9/2024/4/sol1.rb
@@ -5,31 +5,25 @@ $get.day(4)
 
 input = File.read($o[:test] ? 'example.txt' : 'input.txt')
 
-matrix = input.each_line.map(&:strip).map {_1.split('').to_a}
-counter = 0
+m = input.each_line.map(&:strip).map {_1.split('').to_a}
+res = 0
 
-matrix.each_with_index do |row, i|
-  row.each_with_index do |cell, j|
+m.each_with_index do |r, i|
+  r.each_with_index do |c, j|
     begin
-      counter += 1 if matrix[i][j] == 'X' && matrix[i][j+1] == 'M' && matrix[i][j+2] == 'A' && matrix[i][j+3] == 'S'
-      counter += 1 if matrix[i][j] == 'X' && matrix[i+1][j] == 'M' && matrix[i+2][j] == 'A' && matrix[i+3][j] == 'S'
-      counter += 1 if matrix[i][j] == 'S' && matrix[i][j+1] == 'A' && matrix[i][j+2] == 'M' && matrix[i][j+3] == 'X'
-      counter += 1 if matrix[i][j] == 'S' && matrix[i+1][j] == 'A' && matrix[i+2][j] == 'M' && matrix[i+3][j] == 'X'
+      res += 1 if m[i][j] == 'X' && m[i][j+1] == 'M' && m[i][j+2] == 'A' && m[i][j+3] == 'S'
+      res += 1 if m[i][j] == 'X' && m[i+1][j] == 'M' && m[i+2][j] == 'A' && m[i+3][j] == 'S'
+      res += 1 if m[i][j] == 'S' && m[i][j+1] == 'A' && m[i][j+2] == 'M' && m[i][j+3] == 'X'
+      res += 1 if m[i][j] == 'S' && m[i+1][j] == 'A' && m[i+2][j] == 'M' && m[i+3][j] == 'X'
 
-      counter += 1 if matrix[i][j] == 'X' && matrix[i+1][j+1] == 'M' && matrix[i+2][j+2] == 'A' && matrix[i+3][j+3] == 'S'
-      counter += 1 if matrix[i][j] == 'S' && matrix[i+1][j+1] == 'A' && matrix[i+2][j+2] == 'M' && matrix[i+3][j+3] == 'X'
-      counter += 1 if matrix[i][j+3] == 'X' && matrix[i+1][j+2] == 'M' && matrix[i+2][j+1] == 'A' && matrix[i+3][j] == 'S'
-      counter += 1 if matrix[i][j+3] == 'S' && matrix[i+1][j+2] == 'A' && matrix[i+2][j+1] == 'M' && matrix[i+3][j] == 'X'
-    rescue Exception => exception
-      puts exception
+      res += 1 if m[i][j] == 'X' && m[i+1][j+1] == 'M' && m[i+2][j+2] == 'A' && m[i+3][j+3] == 'S'
+      res += 1 if m[i][j] == 'S' && m[i+1][j+1] == 'A' && m[i+2][j+2] == 'M' && m[i+3][j+3] == 'X'
+      res += 1 if m[i][j+3] == 'X' && m[i+1][j+2] == 'M' && m[i+2][j+1] == 'A' && m[i+3][j] == 'S'
+      res += 1 if m[i][j+3] == 'S' && m[i+1][j+2] == 'A' && m[i+2][j+1] == 'M' && m[i+3][j] == 'X'
+    rescue Exception => e
+      # puts e
     end
   end
 end
 
-ap counter
-
-
-# input.scan(/mul\((\d+),(\d+)\)/).map { _1.map(&:to_i) }
-#      .map { _1.first * _1.last }
-#      .sum
-#      .tap {ap _1}
+ap res
--- m9/2024/4/sol2.rb
+++ m9/2024/4/sol2.rb
@@ -5,26 +5,20 @@ $get.day(4)
 
 input = File.read($o[:test] ? 'example.txt' : 'input.txt')
 
-matrix = input.each_line.map(&:strip).map {_1.split('').to_a}
-counter = 0
+m = input.each_line.map(&:strip).map {_1.split('').to_a}
+res = 0
 
-matrix.each_with_index do |row, i|
-  row.each_with_index do |cell, j|
+m.each_with_index do |r, i|
+  r.each_with_index do |c, j|
     begin
-      counter +=1 if matrix[i][j] == 'M' && matrix[i][j+2] == 'S' && matrix[i+1][j+1] == 'A' && matrix[i+2][j] == 'M' && matrix[i+2][j+2] == 'S'
-      counter +=1 if matrix[i][j] == 'S' && matrix[i][j+2] == 'M' && matrix[i+1][j+1] == 'A' && matrix[i+2][j] == 'S' && matrix[i+2][j+2] == 'M'
-      counter +=1 if matrix[i][j] == 'M' && matrix[i][j+2] == 'M' && matrix[i+1][j+1] == 'A' && matrix[i+2][j] == 'S' && matrix[i+2][j+2] == 'S'
-      counter +=1 if matrix[i][j] == 'S' && matrix[i][j+2] == 'S' && matrix[i+1][j+1] == 'A' && matrix[i+2][j] == 'M' && matrix[i+2][j+2] == 'M'
-    rescue Exception => exception
-      puts exception
+      res +=1 if m[i][j] == 'M' && m[i][j+2] == 'S' && m[i+1][j+1] == 'A' && m[i+2][j] == 'M' && m[i+2][j+2] == 'S'
+      res +=1 if m[i][j] == 'S' && m[i][j+2] == 'M' && m[i+1][j+1] == 'A' && m[i+2][j] == 'S' && m[i+2][j+2] == 'M'
+      res +=1 if m[i][j] == 'M' && m[i][j+2] == 'M' && m[i+1][j+1] == 'A' && m[i+2][j] == 'S' && m[i+2][j+2] == 'S'
+      res +=1 if m[i][j] == 'S' && m[i][j+2] == 'S' && m[i+1][j+1] == 'A' && m[i+2][j] == 'M' && m[i+2][j+2] == 'M'
+    rescue Exception => e
+      # puts e
     end
   end
 end
 
-ap counter
-
-
-# input.scan(/mul\((\d+),(\d+)\)/).map { _1.map(&:to_i) }
-#      .map { _1.first * _1.last }
-#      .sum
-#      .tap {ap _1}
+ap res
