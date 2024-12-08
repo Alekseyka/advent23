cf059deea39319e6ecb222a1eb8523619d4f269e
Leks <leks@creditninja.com>
2024-12-08 01:08:37 -0500

    + 2024.08(1,2)

---
+++ oleksiyka/2024/day08/run1.rb
@@ -0,0 +1,66 @@
+nodes = {}
+map = []
+
+def print_map(map)
+  map.each { |i| puts i.join }
+end
+
+def c_2d_to_1d(pos, cols)
+  pos[0] * cols + pos[1]
+end
+
+def c_1d_to_2d(pos, cols)
+  [(pos / cols).to_i, (pos % cols).to_i]
+end
+
+File.foreach('in.txt').with_index do |line, line_index|
+  line = line.sub("\n", '')
+  map.push(line.chars)
+
+  line.chars.each_with_index do |char, row_index|
+    nodes[char] = nodes[char].to_a.push([line_index, row_index]) unless char == '.'
+  end
+
+end
+
+rows = map.size
+cols = map[0].size
+
+q = 0
+
+antinodes = []
+nodes.each do |char, locations|
+  locations.combination(2).to_a.each do |pair|
+    p '----------bla----------'
+    p "Char #{char}, pair #{pair}"
+    pair0 = pair[0]
+    pair1 = pair[1]
+    dcols = pair0[0] - pair1[0]
+    drows = pair0[1] - pair1[1]
+    marker = [pair1[0] - dcols, pair1[1] - drows]
+    p "MD: #{marker}"
+    if marker[0] >= 0 && marker[1] >= 0 && marker[0] < cols && marker[1] < rows
+      if map[marker[0]][marker[1]] != char
+        map[marker[0]][marker[1]] = '#'
+        antinodes.push(marker)
+        p "ANTINODE: #{marker}"
+      end
+    end
+    marker = [pair0[0] + dcols, pair0[1] + drows]
+    p "MU: #{marker}"
+    if marker[0] >= 0 && marker[1] >= 0 && marker[0] < cols && marker[1] < rows
+      if map[marker[0]][marker[1]] != char
+        map[marker[0]][marker[1]] = '#'
+        antinodes.push(marker)
+        p "ANTINODE: #{marker}"
+      end
+    end
+  end
+end
+
+
+p nodes
+p '---'
+print_map map
+p '---'
+p antinodes.uniq.count
---
+++ oleksiyka/2024/day08/run2.rb
@@ -0,0 +1,79 @@
+nodes = {}
+map = []
+
+def print_map(map)
+  map.each { |i| puts i.join }
+end
+
+def c_2d_to_1d(pos, cols)
+  pos[0] * cols + pos[1]
+end
+
+def c_1d_to_2d(pos, cols)
+  [(pos / cols).to_i, (pos % cols).to_i]
+end
+
+antinodes = []
+
+File.foreach('in.txt').with_index do |line, line_index|
+  line = line.sub("\n", '')
+  map.push(line.chars)
+
+  line.chars.each_with_index do |char, row_index|
+    unless char == '.'
+      nodes[char] = nodes[char].to_a.push([line_index, row_index])
+      antinodes.push([line_index, row_index])
+    end
+  end
+
+end
+
+rows = map.size
+cols = map[0].size
+
+nodes.each do |char, locations|
+  locations.combination(2).to_a.each do |pair|
+    p '----------bla----------'
+    p "Char #{char}, pair #{pair}"
+    pair0 = pair[0]
+    pair1 = pair[1]
+    dcols = pair0[0] - pair1[0]
+    drows = pair0[1] - pair1[1]
+    iteratable_cols = pair1[0]
+    iteratable_rows = pair1[1]
+    while true do
+      iteratable_cols -= dcols
+      iteratable_rows -= drows
+      break if iteratable_cols < 0 || iteratable_rows < 0 || iteratable_cols >= cols || iteratable_rows >= rows
+      marker = [iteratable_cols, iteratable_rows]
+      p "MD: #{marker}"
+      if map[marker[0]][marker[1]] != char
+        map[marker[0]][marker[1]] = '#'
+        antinodes.push(marker)
+        p "ANTINODE: #{marker}"
+      end
+    end
+
+    iteratable_cols = pair0[0]
+    iteratable_rows = pair0[1]
+    while true do
+      iteratable_cols += dcols
+      iteratable_rows += drows
+      break if iteratable_cols < 0 || iteratable_rows < 0 || iteratable_cols >= cols || iteratable_rows >= rows
+      marker = [iteratable_cols, iteratable_rows]
+      p "MU: #{marker}"
+      if map[marker[0]][marker[1]] != char
+        map[marker[0]][marker[1]] = '#'
+        antinodes.push(marker)
+        p "ANTINODE: #{marker}"
+      end
+    end
+  end
+end
+
+
+p nodes
+p '---'
+print_map map
+p '---'
+p antinodes.uniq.count
